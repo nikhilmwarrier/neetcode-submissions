0d9f33200d90157681aac4e89adf1179606df9dc
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2026-05-31 13:55:32 +0530

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class DynamicArray {
+public:
+    int *arr = nullptr;
+    int cap = 1;
+    int size = 0;
+
+    DynamicArray(int capacity) {
+        arr = (int *) malloc(sizeof(int) * capacity);
+        if (arr == nullptr) {
+            free(arr);
+            exit(-1);
+        }
+        cap = capacity;
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+        if (i+1 > size)
+            size = i+1;
+    }
+
+    void pushback(int n) {
+        if (size >= cap)
+            resize();
+
+        set(size, n);
+    }
+
+    int popback() {
+        return arr[--size];
+    }
+
+    void resize() {
+        int *tmp = (int *) realloc(arr, 2 * cap * sizeof(int));
+        if (tmp == nullptr) {
+            free(tmp);
+            exit(-1);
+        }
+
+        arr = tmp;
+        cap = cap * 2;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return cap;
+    }
+};
